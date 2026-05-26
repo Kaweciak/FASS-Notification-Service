34a5c5737b1afb8a88597411742afa71cf8693f9
Kacper Kawecki <kacperk343@gmail.com>
2026-05-26 15:11:23 +0200

    added temporary test

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from app.consumer import KafkaEventConsumer
 from app.schemas import KafkaEvent
 from app.router import route_event
 
+print("NEW VERSION OF MAIN.PY LOADED")
+
 app = FastAPI()
 
 consumer = KafkaEventConsumer()
