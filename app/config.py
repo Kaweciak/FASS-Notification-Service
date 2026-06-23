860ba7af03366b5d3d530035632b312166f9f162
Kacper Kawecki <kacperk343@gmail.com>
2026-06-23 15:47:43 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+from pydantic import field_validator
 from pydantic_settings import BaseSettings
 
 
@@ -5,7 +8,7 @@ class Settings(BaseSettings):
     APP_NAME: str = "fass-notification-service"
 
     KAFKA_BOOTSTRAP_SERVERS: str = "kafka:9092"
-    KAFKA_CONSUME_TOPICS: str = "tourist.registered"
+    KAFKA_CONSUME_TOPICS: list[str] = ["tourist.registered"]
     KAFKA_GROUP_ID: str = "email-notification-group"
 
     SMTP_HOST: str = "mailhog"
@@ -14,8 +17,16 @@ class Settings(BaseSettings):
     SMTP_PASSWORD: str = ""
     SMTP_FROM_EMAIL: str = "noreply@example.com"
 
+    @field_validator("KAFKA_CONSUME_TOPICS", mode="before")
+    @classmethod
+    def assemble_topics(cls, v: Union[str, list[str]]) -> list[str]:
+        if isinstance(v, str):
+            return [topic.strip() for topic in v.split(",")]
+        return v
+
     class Config:
         env_file = ".env"
+        extra = "ignore"
 
 
 settings = Settings()
